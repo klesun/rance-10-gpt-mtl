4d79d663f3df13f33856b853d8314b71f4bddda5
klesun <arturklesun@gmail.com>
2025-10-14 23:24:59 +0300

    Add script to parse .ain txt files

---
+++ parse_ain_txt.js
@@ -0,0 +1,37 @@
+import fs from "fs/promises";
+
+const ainTxt = await fs.readFile("./Rance10.ain.txt", "utf-8");
+const lines = ainTxt.split("\n");
+
+const unparsed = [];
+const parsed = [];
+
+function unquote(quoted) {
+    return JSON.parse(quoted.replace(/\t/g, "\\t"));
+}
+
+for (const line of lines) {
+    if (!line.trim()) {
+        continue;
+    }
+    const match = line.match(/^;([ms])\[(\d+)]\s*=\s*(".*")$/);
+    if (match) {
+        const [, lineKind, lineNumber, quotedText] = match;
+        if (quotedText.includes("%")) {
+            // ;s[2259] = "*	%s = %f\n"
+            // apparently this text may use variables
+            unparsed.push(line);
+            continue;
+        }
+        const text = unquote(quotedText);
+        parsed.push({ lineKind, lineNumber, text });
+    } else {
+        unparsed.push(line);
+    }
+}
+
+const result = { parsed, unparsed };
+const messageLines = parsed.filter(lr => lr.lineKind === "m");
+const outputJson = "[\n" + messageLines.map(ml => JSON.stringify(ml)).join(",\n") + "\n]";
+
+await fs.writeFile("./Rance10.ain.json", outputJson, "utf-8");
