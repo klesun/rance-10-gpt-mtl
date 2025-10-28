0b9a8f9902e612c80b7df822dbca5a250d06d75d
Artur Klesun <arturklesun@gmail.com>
2025-10-28 11:52:21 +0000

    Add jsdoc comment for function signatures

--- modules/AinTxtParser.js
+++ modules/AinTxtParser.js
@@ -3,6 +3,21 @@ function unquote(quoted) {
     return JSON.parse(quoted.replace(/\t/g, "\\t"));
 }
 
+/**
+ * @param ainTxt = `
+ * ; activity::detail::CActivity@LoadActivity
+ * ;s[8] = "アクティビティの読み込みに失敗しました【%s】"
+ * ;m[63840] = "荒々しくハイパー兵器を捻り込んだ。"
+ * @return {{
+ *     unparsed: string[],
+ *     parsed: {
+ *         lineKind: "m" | "s",
+ *         lineNumber: number,
+ *         commented: boolean,
+ *         originalJapaneseLine: "「なーーーーにを上から見下ろしてるかーーー！」" | string,
+ *     }[],
+ * }}
+ */
 export default function parseAin(ainTxt) {
     const unparsed = [];
     const parsed = [];
@@ -16,7 +31,7 @@ export default function parseAin(ainTxt) {
         if (match) {
             const [, commented, lineKind, lineNumber, quotedText] = match;
             const originalJapaneseLine = unquote(quotedText);
-            parsed.push({ lineKind, lineNumber, commented: !!commented, originalJapaneseLine });
+            parsed.push({ lineKind, lineNumber: +lineNumber, commented: !!commented, originalJapaneseLine });
         } else {
             unparsed.push(line);
         }
